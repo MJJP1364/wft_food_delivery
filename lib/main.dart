e42d7c95ec1d11fa46b12f267446e10e12eded55
MJJP1364 <m.j.j.p1364@gmail.com>
2025-05-19 20:21:27 +0330

    home edition

--- lib/main.dart
+++ lib/main.dart
@@ -35,6 +35,8 @@ void main() async {
     statusBarColor: Colors.transparent,
     statusBarIconBrightness: Brightness.dark,
   );
+
+  //--------------------------------vvvvvvvvvvv12
  
 
   // runApp(

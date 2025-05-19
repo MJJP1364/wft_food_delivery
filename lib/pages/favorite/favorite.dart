c49b1a027c4c02e2c0f4d2422179ba8158302008
m.javad javadpoor <m.j.j.p1364@gmail.com>
2025-05-19 11:28:52 +0330

    add hero to favorite

--- lib/pages/favorite/favorite.dart
+++ lib/pages/favorite/favorite.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:wft_food_delivery_code/Core/models/product_model.dart';
 import 'package:wft_food_delivery_code/pages/favorite/favorit_controller.dart';
+import 'package:wft_food_delivery_code/pages/viewAll_Screen/food_details_screen.dart';
 import 'package:wft_food_delivery_code/pages/viewAll_Screen/view_all_screen_controller.dart';
 import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 
@@ -45,63 +46,72 @@ class FavoritePage extends StatelessWidget {
                   verticalOffset: 50.0,
 
                   child: FadeInAnimation(
-                    child: Container(
-                      margin: const EdgeInsets.symmetric(
-                        vertical: 8,
-                        horizontal: 16,
-                      ),
-                      padding: const EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        color: Colors.red[50],
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      child: Row(
-                        children: [
-                          // تصویر محصول
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(12),
-                            child: CachedNetworkImage(
-                              imageUrl: product.imageCard,
-                              width: 80,
-                              height: 80,
-                              fit: BoxFit.fill,
-                              placeholder:
-                                  (context, url) =>
-                                      const CircularProgressIndicator(),
-                              errorWidget:
-                                  (context, url, error) =>
-                                      const Icon(Icons.error),
-                            ),
-                          ),
-
-                          const SizedBox(width: 20),
-
-                          // نام محصول
-                          Expanded(
-                            child: Text(
-                              product.name,
-                              style: const TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black,
+                    child: GestureDetector(
+                      onTap:
+                          () =>
+                              Get.to(() => FoodDetailScreen(product: product)),
+                      child: Container(
+                        margin: const EdgeInsets.symmetric(
+                          vertical: 8,
+                          horizontal: 16,
+                        ),
+                        padding: const EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          color: Colors.red[50],
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: Row(
+                          children: [
+                            // تصویر محصول
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(12),
+                              child: Hero(
+                                tag: product.id,
+                                transitionOnUserGestures: true,
+                                child: CachedNetworkImage(
+                                  imageUrl: product.imageCard,
+                                  width: 80,
+                                  height: 80,
+                                  fit: BoxFit.fill,
+                                  placeholder:
+                                      (context, url) =>
+                                          const CircularProgressIndicator(),
+                                  errorWidget:
+                                      (context, url, error) =>
+                                          const Icon(Icons.error),
+                                ),
                               ),
                             ),
-                          ),
 
-                          // دکمه حذف
-                          IconButton(
-                            onPressed: () {
-                              favoriteController.removeFromFavorites(
-                                favorite.productId,
-                              );
-                            },
-                            icon: Icon(
-                              Icons.delete_forever,
-                              size: 40,
-                              color: Colors.red[700],
+                            const SizedBox(width: 20),
+
+                            // نام محصول
+                            Expanded(
+                              child: Text(
+                                product.name,
+                                style: const TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black,
+                                ),
+                              ),
                             ),
-                          ),
-                        ],
+
+                            // دکمه حذف
+                            IconButton(
+                              onPressed: () {
+                                favoriteController.removeFromFavorites(
+                                  favorite.productId,
+                                );
+                              },
+                              icon: Icon(
+                                Icons.delete_forever,
+                                size: 40,
+                                color: Colors.red[700],
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ),
